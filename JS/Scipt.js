772b530dcef179e3894d7291d39bcae96dc51f23
Ivan9556 <168356668+Ivan9556@users.noreply.github.com>
2025-05-01 18:36:06 +0200

    Carrusel y contenido

--- JS/Scipt.js
+++ JS/Scipt.js
@@ -6,4 +6,22 @@ desplegable.addEventListener('click', ()=> {
     menu.classList.toggle('show'); // Alterna la clase 'show' en el menú
 });
 
-//Otra función
+//Funcion carrusel
+let indice = 0;
+mostrarImagenes();
+function mostrarImagenes(){
+    const img = document.getElementsByClassName("diapositiva");
+    for(let i = 0; i < img.length; i++){
+        //img[i].style.display = "none"; oculta todas las imagenes
+        img[i].classList.remove("visible");
+    }
+    /*
+    indice++;
+    if(indice > img.length) indice = 1;
+    */
+    indice = (indice + 1) % img.length;
+    img[indice].classList.add("visible");
+    //img[indice -1].style.display = "block"; //Muestra solo una
+    setTimeout(mostrarImagenes, 5000) //Cambia cada 3 seg
+}
+
