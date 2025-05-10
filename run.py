d92dd8da267a8b0920ff1e3d808baacb5ca194d4
Ivan9556 <168356668+Ivan9556@users.noreply.github.com>
2025-05-10 11:46:38 +0200

    name

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from app import create_app
 
 app = create_app() #funcion que configura y devuelve la app Flask def en "__init__"
 
-if name == '__main__':
+if __name__ == '__main__':
     app.run(debug=True) #activamos el modo desarrollador 
     
