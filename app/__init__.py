b569ecc885bbe5d64e3fc14d815a7c06ba0473fe
Ivan9556 <168356668+Ivan9556@users.noreply.github.com>
2025-05-10 21:13:15 +0200

    metodo enviar mensajes

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,27 @@
-#Inicio Flask y MongoDB
+"""
+Este archivo se encarga de inicializar la aplicación Flask y sus extensiones.
+
+- Crea una instancia de Flask.
+- Carga la configuración desde un archivo externo (config.py).
+- Inicializa extensiones como PyMongo, Flask-Mail, etc.
+- Registra Blueprints para organizar las rutas por módulos.
+
+"""
+
 from flask import Flask
 from flask_pymongo import PyMongo #extensión para conectar Flask con MongoDB.
+from flask_mail import Mail
+
 
 mongo = PyMongo()
+mail =  Mail()
 
 def create_app():
     app = Flask(__name__)  
     app.config.from_pyfile('../config.py') #Lee la configuracion desde config
     #Este método nos permite vincular la app
     mongo.init_app(app) #Inicia MongoDB
+    mail.init_app(app)
 
     from .route import main
     app.register_blueprint(main) # dividir rutas em varios archivos
--- app/route.py
+++ app/route.py
@@ -1,7 +1,11 @@
-#Rutas de la app
-from flask import Blueprint, render_template
-
 """
+Este archivo define las rutas (endpoints) de la aplicación web.
+
+Cada función decorada con @main.route corresponde a una URL específica.
+
+Aquí se maneja la lógica para renderizar páginas HTML con `render_template`, 
+gestionar formularios mediante `request`, y realizar acciones como enviar correos o acceder a la base de datos.
+
 Blueprint permite organizar una aplicación Flask en componentes reutilizables y modulares.
 
 'main': Es el nombre del Blueprint. Se usa luego para 
@@ -9,8 +13,12 @@ registrar rutas y para llamar con url_for('main.nombre_función').
 
 __name__: Indica el módulo actual. Flask lo usa para localizar archivos 
 relacionados (por ejemplo, plantillas o archivos estáticos si se personaliza).
-
 """
+from flask import Blueprint, render_template, url_for, flash
+from flask_mail import Message
+from . import mail #Iniciandolo previamente en __init__.py
+
+
 main = Blueprint('main' ,__name__)
 
 @main.route('/') #se carga desde la raiz index.html
@@ -32,3 +40,24 @@ def queHacer():
 @main.route('/encuentranos')
 def encuentranos():
     return render_template('encuentranos.html')
+
+@main.route('/encuentranos', methods=['POST'])
+def enviar_mensaje():
+    nombre = request.form.get('nombre') 
+    correo = request.form.get('correo')
+    asunto = request.form.get('asunto')
+    mensaje = request.form.get('mensaje')
+
+    if not nombre or not correo:
+        flash('Algunos campos son obligatorios')
+        return redirect(url_for('main.inicio'))
+    
+    msg = Message(
+        subject = asunto,
+        sender = correo,
+        recipients = [ejemplo_cuevas@hotmail.com],
+        body = mensaje
+    )
+    mail.send(msg)
+    flash('Mensaje enviado')
+    return redirect(url_for('main.inicio'))
