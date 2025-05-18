96f9582301516f258d04b0c1c35f6ea15286be07
Ivan9556 <168356668+Ivan9556@users.noreply.github.com>
2025-05-18 18:46:31 +0200

    Fotos nuevas

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
-#Lógica bd
+#Clase cueva
+class cueva:
+    def __init__(self, id ,nombre, huespedes, precio):
+        self.id = id
+        self.nombre = nombre
+        self.huespedes = huespedes
+        self.precio = precio
+    def to_dict(self):
+        return {
+        "id" : self.id,
+        "nombre" : self.nombre,
+        "huespedes" : self.huespedes,
+        "precio" :  self.precio
+        }        
