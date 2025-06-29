a1b9eccbf0b0342f25d3367bd1ac76483a312ae0
Ivan9556 <168356668+Ivan9556@users.noreply.github.com>
2025-06-30 01:33:00 +0200

    carrusel de platilla cueva

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -8,19 +8,18 @@ desplegable.addEventListener('click', () => {
 
 //Funcion carrusel
 const img = document.querySelectorAll('.carrusel img');
+
 let indice = 0;
-function siguienteImagen() {
-    //Quita la clase "active" a todas para ocultarlas (opacity: 0 en CSS)
-    img.forEach(img => img.classList.remove('active'));
-    //Añade la clase "active" hace visible la img (opacity: 1 con transición suave) 
-    img[indice].classList.add('active');
-    //% (módulo) sirve para volver a 0 
-    indice = (indice + 1) % img.length;
+
+if(img.length>0){
+    function siguienteImagen() {
+        //Quita la clase "active" a todas para ocultarlas (opacity: 0 en CSS)
+        img.forEach(img => img.classList.remove('active'));
+        //Añade la clase "active" hace visible la img (opacity: 1 con transición suave) 
+        img[indice].classList.add('active');
+        //% (módulo) sirve para volver a 0 
+        indice = (indice + 1) % img.length;
+    }
+    siguienteImagen();
+    setInterval(siguienteImagen, 10000);
 }
-siguienteImagen();
-setInterval(siguienteImagen, 10000);
-
-//Carrusel cuevas
-
-
-
