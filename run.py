415c2bab3e4a0e497276815261d908daaeb1525b
Ivan9556 <168356668+Ivan9556@users.noreply.github.com>
2025-07-01 20:50:47 +0200

    push koyeb

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import create_app
 app = create_app() #funcion que configura y devuelve la app Flask def en "__init__"
 
 if __name__ == '__main__':
-    app.run(debug=True) #activamos el modo desarrollador 
+    app.run(host="0.0.0.0", port=8000, debug=True) #activamos el modo desarrollador 
     
